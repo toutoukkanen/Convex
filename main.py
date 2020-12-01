06749c53d4ab65083d89e6d469d3f4d005339401
toutoukkanen <tuukka.007@hotmail.com>
2020-12-02 00:44:17 +0200

    Created a function for rotating objects around z-axis. Also made defining coordinates a bit easier and more intuitive.

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 import matplotlib.pyplot as plt
+from math import sin
+from math import cos
+from math import pi
 
 
-def vector_mult(x=[0] * 2, y=[0] * 2, z=[0] * 2):
+# "Moves" origo to mass_center and calculates there.
+# Then moves origo back and returns new values
+def rotate_around_z(x: list, y: list, mass_center, angle_radians):
+    x_new = []
+    y_new = []
+
+    # Calculate coords relative to mass center
+    x_relative = [x_origo - mass_center[0] for x_origo in x]
+    y_relative = [y_origo - mass_center[1] for y_origo in y]
+
+    # Now rotate around z-axis
+    for valueX, valueY in zip(x_relative, y_relative):
+        x_new.append(valueX * cos(angle_radians) - valueY * sin(angle_radians))
+        y_new.append(valueX * sin(angle_radians) + valueY * cos(angle_radians))
+
+    # Transform back origo-centric format
+    x_new = [x_relative + mass_center[0] for x_relative in x_new]
+    y_new = [y_relative + mass_center[1] for y_relative in y_new]
+
+    # print("Origo", x)
+    # print("Relative", x_relative)
+    # print("New", x_new)
+
+    return x_new, y_new
+
+
+def vector_mult(x=[0] * 2, y=[0] * 2, z=[0] * 2):  # 0 as default argument so no out of bounds
+    # Example of vector multiplication
+    # i, j, k = vector_mult(x, y, z)
+    # print("v1 x v2 =", vector_mult(x,y,z))
+
     i = y[0] * z[1] - z[0] * y[1]
     j = z[0] * x[1] - x[0] * z[1]
     k = x[0] * y[1] - y[0] * x[1]
@@ -9,33 +42,60 @@ def vector_mult(x=[0] * 2, y=[0] * 2, z=[0] * 2):
     return i, j, k
 
 
-def make_xyz_map(v: list):
+# Given a list of coordinates, return them in component lists of x,y,z
+def make_xyz_map(coordinates: list):
     x = []
     y = []
     z = []
 
     # Construct lists of individual components
-    for component in v:
-        x.append(component[0])
-        y.append(component[1])
-        z.append(component[2])
+    for coord_tuple in coordinates:
+        x.append(coord_tuple[0])
+        y.append(coord_tuple[1])
+        z.append(coord_tuple[2])
 
     return x, y, z
 
 
-# Define dots (or vectors)
-v1 = (-0.1, 0.2, 0)
-v2 = (0.71, -0.71, 0)
+def main():
+    # Define dots (or vectors)
+    # v1 = (-0.1, 0.2, 0)
+    # v2 = (0.71, -0.71, 0)
 
-x, y, z = make_xyz_map([v1, v2])
+    # A triangle
+    # coordinates = [
+    #     (1.5, 0, 0),
+    #     (-1, 0.5, 0),
+    #     (-1, -0.5, 0),
+    #     (1.5, 0, 0)  # Last dot to complete the triangle
+    # ]
 
-# Makes the same list as
-# x = [-0.1, 0.71]
-# y = [0.2, -0.71]
-# y = [0, 0]
+    # A cube
+    coordinates = [
+        (0, 0, 0),
+        (1, 0, 0),
+        (1, 1, 0),
+        (0, 1, 0),
+        (0, 0, 0)
+    ]
 
-print("Coordinate lists:", x, y, z)
+    cm = (0.5, 0.5)  # Mass center
 
-# Example of vector multiplication
-# i, j, k = vector_mult(x, y, z)
-print("v1 x v2 =", vector_mult(x,y,z))
+    rotate_radians = pi / 4
+
+    x, y, z = make_xyz_map(coordinates)
+
+    print("Coordinate lists:", x, y, z)
+
+    plt.plot(x, y)
+
+    x, y = rotate_around_z(x, y, cm, rotate_radians)
+
+    plt.plot(x, y)
+    plt.show()
+
+
+main()
+
+
+
