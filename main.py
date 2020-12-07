87c31e303ee1509718993d7e824d268dc80c0939
toutoukkanen <tuukka.007@hotmail.com>
2020-12-07 20:09:30 +0200

    Fixed vector multiplication returning wrong value.

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def vector_mult(vector1, vector2):
     result = [0, 0, 0]
 
     result[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1]
-    result[1] = vector1[2] * vector1[0] - vector1[0] * vector2[2]
+    result[1] = -(vector1[0] * vector2[2] - vector1[2] * vector2[0])
     result[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0]
 
     return result
